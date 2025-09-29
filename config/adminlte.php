a6a8e6f61c9af1cf5c90bee1a91fbc7d64db0253
Manoj Bandara <manojbandara12@gmail.com>
2025-09-29 15:44:13 +0530

    Fix - App logo and redirection

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,8 +63,8 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<b style="color: white;">AHQAMS</b>',
+    'logo_img' => 'login_assets/img/Army_Logo_my.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
@@ -257,7 +257,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'dashboard',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
